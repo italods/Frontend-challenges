0210658e9e926a79ec9a2a490d190c4d3e2752fe
Italo Pereira <italopereira007@hotmail.com>
2021-09-14 22:05:19 -0300

    .

---
+++ PokemonCardGenerator/script.js
@@ -0,0 +1,28 @@
+const typeColor = {
+  bug: "#26de81",
+  dragon: "#ffeaa7",
+  electric: "#fed330",
+  fairy: "#FF0069",
+  fighting: "#30336b",
+  fire: "#f0932b",
+  flying: "#81ecec",
+  grass: "#00b894",
+  ground: "#EFB549",
+  ghost: "#a55eea",
+  ice: "#74b9ff",
+  normal: "#95afc0",
+  poison: "#6c5ce7",
+  psychic: "#a29bfe",
+  rock: "#2d3436",
+  water: "#0190FF",
+};
+
+const url = "https://pokeapi.co/api/v2/pokemon/";
+const card = document.getElementById("card");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", getPokeData);
+
+let getPokeData = () => {
+  // Gerando numero aleatorio 
+}
