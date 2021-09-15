259d7342fefe3892c9a8907265b8f999f8317e26
Italo Pereira <italopereira007@hotmail.com>
2021-09-14 23:10:38 -0300

    .

--- PokemonCardGenerator/script.js
+++ PokemonCardGenerator/script.js
@@ -16,7 +16,6 @@ const typeColor = {
   rock: "#2d3436",
   water: "#0190FF",
 };
-
 const url = "https://pokeapi.co/api/v2/pokemon/";
 const card = document.getElementById("card");
 const btn = document.getElementById("btn");
@@ -43,7 +42,56 @@ let generateCard = (data) => {
   const hp = data.stats[0].base_stat;
   const imgSrc = data.sprites.other.dream_world.front_default;
   const pokeName = data.name;
-  const statAttack = data.stats[1].base_stats;
+  const statAttack = data.stats[1].base_stat;
+  const statDefense = data.stats[2].base_stat;
+  const statSpeed = data.stats[5].base_stat;
+
+  // Setando a cor de acordo com o tipo do pokemon.
+  const themeColor = typeColor[data.types[0].type.name];
+
+  card.innerHTML = `
+      <p class="hp">
+        <span>HP</span>
+        ${hp}
+      </p>
+      <img src="${imgSrc}" />
+      <h2 class="poke-name">${pokeName}</h2>
+      <div class="types">
+        
+      </div>
+      <div class="stats">
+        <div>
+          <h3>${statAttack}</h3>
+          <p>Attack</p>
+        </div>
+        <div>
+          <h3>${statDefense}</h3>
+          <p>Defense</p>
+        </div>
+        <div>
+          <h3>${statSpeed}</h3>
+          <p>Speed</p>
+        </div>
+      </div>
+  `;
+
+  appendTypes(data.types);
+  styleCard(themeColor);
+}
+let appendTypes = (types) => {
+  console.log(types);
+  types.forEach((item) => {
+    let span = document.createElement("SPAN");
+    span.textContent = item.type.name;
+    document.querySelector(".types").appendChild(span);
+  })
+};
+let styleCard = (color) => {
+  card.style.background = `background: radial-gradient(
+    circle at 50% 0%, 
+    ${color} 36%, 
+    #ffffff 36%
+  )`
 }
 
 btn.addEventListener("click", getPokeData);
